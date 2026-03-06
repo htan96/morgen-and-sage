df8d788d2dcfd71daedd9652a276bd6c0b55bd07
Harold Tan <htan96@icloud.com>
2026-03-06 10:59:14 -0800

    switch to playwright for pdf generation

--- lib/invoices/generateInvoicePdf.ts
+++ lib/invoices/generateInvoicePdf.ts
@@ -1,25 +1,21 @@
-import chromium from "@sparticuz/chromium";
-import puppeteer from "puppeteer-core";
+import { chromium } from "playwright-core";
 
 export async function generateInvoicePdf(invoiceId: string) {
 
-  const browser = await puppeteer.launch({
-    args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
+  const browser = await chromium.launch({
+    headless: true,
   });
 
   const page = await browser.newPage();
 
   await page.goto(
     `${process.env.NEXT_PUBLIC_APP_URL}/reports/invoice/${invoiceId}?print=true`,
-    { waitUntil: "networkidle0" }
+    { waitUntil: "networkidle" }
   );
 
   const pdf = await page.pdf({
     format: "A4",
     printBackground: true,
-    preferCSSPageSize: true,
   });
 
   await browser.close();
